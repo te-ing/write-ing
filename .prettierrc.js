57e3c22db02997d61d898e0e9f57f655e0921710
taejung.kim <taejung.kim@snplab.io>
2022-11-28 14:26:19 +0900

    chore: lint 설치

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  proseWrap: 'preserve',
+};
