aff4df52c9eb3240999cd5dadf2c586c576aa9d9
taejung.kim <taejung.kim@snplab.io>
2022-12-05 11:32:41 +0900

    feat: global style 및 color palette 구현

--- client/app/edit/layout.tsx
+++ client/app/edit/layout.tsx
@@ -1,5 +1,3 @@
-import '@/app/globals.scss';
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <div>{children}</div>;
 }
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import '@styles/globalStyles.scss';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
