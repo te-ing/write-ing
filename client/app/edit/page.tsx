b16cc0eadcf11a459d4e3c891e40385ddd0478d2
taejung.kim <taejung.kim@snplab.io>
2023-01-05 14:18:19 +0900

    fix: build를 위한 에러 수정

--- client/app/edit/page.tsx
+++ client/app/edit/page.tsx
@@ -1,53 +1,35 @@
 'use client';
-import React, { useEffect, useRef } from 'react';
-import { Editor } from '@toast-ui/react-editor';
-import '@toast-ui/editor/dist/toastui-editor.css';
-import 'tui-color-picker/dist/tui-color-picker.css';
-import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
-import 'prismjs/themes/prism.css';
-import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
-import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js';
-import colorSyntax from '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.js';
+import React, { Suspense, useEffect, useRef, useState } from 'react';
 import styles from './edit.module.scss';
 import { createPost } from 'api/posts.api';
 import { useForm } from 'react-hook-form';
+import dynamic from 'next/dynamic';
+
+interface FormValues {
+  title: string;
+  subtitle?: string;
+  nickname: string;
+  status: string;
+  content: string;
+  category?: string;
+  tag?: string;
+}
+
+const ToastEditor = dynamic(() => import('components/ToastEditor'), {
+  ssr: false,
+});
 
 export default function EditPage() {
   const editorRef = useRef(null);
-  const toolbarItems = [
-    ['heading', 'bold', 'italic', 'strike'],
-    ['hr'],
-    ['ul', 'ol', 'task'],
-    ['table', 'link'],
-    ['image'],
-    ['code'],
-    ['scrollSync'],
-  ];
-
-  interface FormValues {
-    title: string;
-    subtitle?: string;
-    nickname: string;
-    status: string;
-    content: string;
-    category?: string;
-    tag?: string;
-  }
-
   const {
     register,
     handleSubmit,
     formState: { isSubmitting, isDirty, errors },
     setError,
   } = useForm<FormValues>();
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    const initTerminal = async () => {
-      const { Terminal } = await import('xterm');
-      const term = new Terminal();
-      // Add logic with `term`
-    };
-    initTerminal();
+    setLoading(false);
   }, []);
 
   const onSubmit = async ({ title }: FormValues) => {
@@ -68,18 +50,11 @@ export default function EditPage() {
         <div className={styles.title}>
           <input {...register('title')} type="text" className={styles.title_input} placeholder="제목을 입력하세요" />
         </div>
-        <div className={styles.editor}>
-          <Editor
-            ref={editorRef}
-            initialValue="" // 글 수정 시 사용
-            initialEditType="markdown" // wysiwyg & markdown
-            hideModeSwitch={true}
-            height="500px"
-            usageStatistics={false}
-            toolbarItems={toolbarItems}
-            plugins={[colorSyntax, codeSyntaxHighlight]}
-          />
-        </div>
+        <Suspense fallback={<p>로딩..</p>}>
+          <div className={styles.editor} resource={''}>
+            <ToastEditor editorRef={editorRef} />
+          </div>
+        </Suspense>
         <button>click</button>
       </form>
     </>
