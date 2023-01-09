3b8b06719cfef0ba3c4910f55de9698cbe0d4c50
taejung.kim <taejung.kim@snplab.io>
2023-01-09 15:15:16 +0900

    feat: 배포를 위한 env 설정

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -5,11 +5,11 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'localhost',
+  host: process.env.HOST,
   port: 3306,
   username: process.env.DB_USER_ID,
   password: process.env.DB_USER_PASSWORD,
-  database: 'mariadb',
+  database: process.env.DB_NAME,
   synchronize: true, // 싱크를 맞추는 과정에서 데이터 유실 가능성이 있기 때문에 운영환경에서는 false
   logging: false,
   entities: ['src/entities/**/*.ts'],
