bab98bb7586e302c7eee4dae479836ed1e97c32f
taejung.kim <taejung.kim@snplab.io>
2023-01-16 16:00:19 +0900

    feat: react-quary 설정 및 구현

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -1,20 +1,26 @@
+'use client';
+
+import axios from 'axios';
 import PostCard from 'components/Post/PostCard';
+import { useQuery } from 'react-query';
 import { PostType } from 'types/post';
 import styles from './page.module.scss';
 
-const getPostList = async (): Promise<PostType[]> => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/post/list`);
-  const data = await response.json();
-  return data;
-};
+export default function HomePage() {
+  const getPostList = async (): Promise<PostType[]> => {
+    const { data } = await axios(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/post/list`);
+    return data;
+  };
 
-export default async function HomePage() {
-  const posts = await getPostList();
+  const { isLoading, isError, data } = useQuery('posts', getPostList);
+
   return (
     <div className={styles.postCard_wrapper}>
-      {posts.map((post) => {
-        return <PostCard post={post} key={post.id} />;
-      })}
+      {isLoading
+        ? 'Loading..'
+        : data.map((post) => {
+            return <PostCard post={post} key={post.id} />;
+          })}
     </div>
   );
 }
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,17 +1,30 @@
+'use client';
+
 import '@styles/globalStyles.scss';
 import Header from 'components/Header/Header';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import styles from './layout.module.scss';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <div className={styles.layout_wrapper}>
-          <div className={styles.layout}>
-            <Header />
-            {children}
+        <QueryClientProvider client={queryClient}>
+          <div className={styles.layout_wrapper}>
+            <div className={styles.layout}>
+              <Header />
+              {children}
+            </div>
           </div>
-        </div>
+        </QueryClientProvider>
       </body>
     </html>
   );
