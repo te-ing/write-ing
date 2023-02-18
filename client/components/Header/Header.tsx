e61ba3764d4c2263e60b7f63ae2db88ca1ac37dc
te-ing <hi2177@naver.com>
2023-02-18 11:11:38 +0900

    style: blog v2 style 환경설정 및 헤더 구현

--- client/components/Header/Header.tsx
+++ client/components/Header/Header.tsx
@@ -1,17 +1,24 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import styles from './/header.module.scss';
+import { Logo } from 'public/image/svg';
+import styles from './header.module.scss';
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <Link href="/">
-        <Image src={'/image/logo.svg'} alt="logo" width={76} height={46} priority />
+        <Logo width={76} height={46} />
       </Link>
-      <div className={styles.Link}>
-        <Link href="/home">Home</Link>
-        <Link href="/">About Me</Link>
-        <Link href="/">Contact</Link>
+      <div className={styles.linkWrapper}>
+        <div className={styles.link}>
+          <Link href="/home">Home</Link>
+          <Link href="/">About Me</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div className={styles.link}>
+          <Link href="/login">Login</Link>
+          <Link href="/edit">Edit</Link>
+        </div>
       </div>
     </header>
   );
