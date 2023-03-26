0b4bbf7cac353b03edf067b6b76b526f63fc1fa2
te-ing <hi2177@naver.com>
2023-03-26 22:29:13 +0900

    feat: 댓글 UI 구현

--- client/components/Comments/Comment.tsx
+++ client/components/Comments/Comment.tsx
@@ -1,26 +1,47 @@
 'use client';
 
+import { MoreIcon } from '@public/image/svg';
 import { deleteComment } from 'api/posts.api';
+import cx from 'classnames';
+import { CommonButton } from 'components/common/inputs/CommonButton/CommonButton';
+import { useState } from 'react';
 import { useMutation, useQueryClient } from 'react-query';
 import { CommentType } from 'types/comment';
+import styles from './Comment.module.scss';
 
 const Comment = ({ comment }: { comment: CommentType }) => {
+  const [isToggleOn, setIsToggleOn] = useState(false);
   const queryClient = useQueryClient();
   const commentDeleteMutation = useMutation(deleteComment, {
     onSuccess: (res) => {
+      alert('삭제되었습니다.');
       queryClient.invalidateQueries('getComment');
     },
   });
 
   const handleDeleteComment = () => {
     commentDeleteMutation.mutate({ postId: comment.postId, commentId: comment.id });
+    setIsToggleOn(false);
   };
   return (
-    <div>
-      댓글
-      <div>닉네임: {comment.nickname}</div>
-      <div>내용: {comment.body}</div>
-      <button onClick={handleDeleteComment}>삭제</button>
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <div className={styles.infoWrapper}>
+          <h4>{comment.nickname}</h4>
+          <span className={styles.date}>{comment.writeTime || '2023. 03. 21. 21:43'}</span>
+        </div>
+        <div className={styles.toggleWrapper}>
+          <button onClick={() => setIsToggleOn(!isToggleOn)}>
+            <MoreIcon />
+          </button>
+          <div className={cx(styles.toggleItem, { [styles.hidden]: !isToggleOn })}>
+            <CommonButton width="60px" onClick={handleDeleteComment}>
+              삭제
+            </CommonButton>
+          </div>
+        </div>
+      </div>
+      <div className={styles.content}>{comment.body}</div>
     </div>
   );
 };
--- client/public/image/svg/index.ts
+++ client/public/image/svg/index.ts
@@ -1,4 +1,5 @@
 import Logo from './Logo.svg';
 import NavArrowIcon from './NavArrowIcon.svg';
+import MoreIcon from './MoreIcon.svg';
 
-export { Logo, NavArrowIcon };
+export { Logo, NavArrowIcon, MoreIcon };
