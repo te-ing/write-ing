75b6e4d0c0b7b362e1ee9c8a058d6027960cf851
te-ing <hi2177@naver.com>
2023-05-05 16:48:22 +0900

    feat: footer 수정

--- client/components/Post/PostDetail/PostDetailFooter.tsx
+++ client/components/Post/PostDetail/PostDetailFooter.tsx
@@ -1,20 +1,15 @@
-import { deletePost } from 'api/posts.api';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 import styles from './PostDetailFooter.module.scss';
 
 const PostDetailFooter = ({ postId }: { postId: number }) => {
   const router = useRouter();
-  const postDelete = async () => {
-    // const result = await deletePost(postId);
-    router.push('/home');
-  };
+  // const postDelete = async () => {
+  //   const result = await deletePost(postId);
+  //   router.push('/home');
+  // };
 
-  return (
-    <div className={styles.wrapper}>
-      PostDetailFooter<button onClick={postDelete}>삭제</button>
-    </div>
-  );
+  return <div className={styles.wrapper}>{/* PostDetailFooter<button onClick={postDelete}>삭제</button> */}</div>;
 };
 
 export default PostDetailFooter;
